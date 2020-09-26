1007fd1358b4d36e463c239c6b7b3202a1887c4d
Ryan <supermarioryan@gmail.com>
2020-09-26 20:08:02 +0100

    > Add reprod

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<script>
+import { h } from "vue";
+
+const Button = (props, context) => {
+  let component = "a";
+
+  if (!props.href) {
+    component = "button";
+  }
+
+  if (!context.slots.default) {
+    return null;
+  }
+
+  return h(
+    component,
+    { ...props, ...context.attr, className: "button" },
+    context.slots.default()
+  );
+};
+
+Button.props = ["href", "to"];
+
+export default Button;
+</script>
+
+<style scoped>
+.button {
+    background: crimson;
+}
+</style>
